1f070398d87671f44f030e79fa284ee9460eff99
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-11-15 12:05:02 +0530

    imported cw module for MSK creation

--- main.tf
+++ main.tf
@@ -11,11 +11,69 @@ terraform {
 
 #TF code for importing IAM Role
 module "iam" {
-  source ="git@github.com:satishkumarkrishnan/Terraform_IAM.git?ref=main"  
+  source ="git@github.com:satishkumarkrishnan/Terraform_IAM.git?ref=main"
 }
 
 module "cw" {
   source ="git@github.com:satishkumarkrishnan/Terraform-CloudWatch.git?ref=main"
+}
+
+# TF code MSK cluster
+resource "aws_msk_cluster" "tokyo_msk_cluster" {
+  cluster_name           = "tokyo_msk_cluster"
+  kafka_version          = "3.2.0"
+  number_of_broker_nodes = 3
+
+  broker_node_group_info {
+    instance_type = "kafka.m5.large"
+    client_subnets = [
+	  module.vpc.vpc_fe_subnet,
+	  module.vpc.vpc_be_subnet,      
+    ]
+    storage_info {
+      ebs_storage_info {
+        volume_size = 1000
+      }
+    }
+    security_groups = [module.vpc.vpc_fe_sg]
+  }
+
+  encryption_info {
+    encryption_at_rest_kms_key_arn = module.kms.kms_arn
+  }
+
+  open_monitoring {
+    prometheus {
+      jmx_exporter {
+        enabled_in_broker = true
+      }
+      node_exporter {
+        enabled_in_broker = true
+      }
+    }
+  }
+
+  logging_info {
+    broker_logs {
+      cloudwatch_logs {
+        enabled   = true
+        log_group = module.cw.cw_log_group
+      }
+#      firehose {
+#        enabled         = true
+#        delivery_stream = aws_kinesis_firehose_delivery_stream.test_stream.name
+#      }
+      s3 {
+        enabled = true
+        bucket  = aws_s3_bucket.kms_encrypted.id
+        prefix  = "logs/msk-"
+      }
+    }
+  }
+
+  tags = {
+    foo = "bar"
+  }
 }
 
 
